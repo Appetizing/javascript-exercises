0b3d9dfeab28edeb933f4746126a74af65c7a966
khang <kvo0731@gmail.com>
2023-12-18 02:22:45 -0700

    Test Cases 1-4
    Completed the first 4 problems

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, count) {
+    let message = '';
+    if(count >= 0) {
+        for(let i = 0; i < count; i++) {
+            message += word;
+        }
+        return message;
+    }
+    else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
